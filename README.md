d3cb42f68b3f605fc554b3f698ff278ae0c359d7
Dmitry Baskakov <dmitry@bask.ws>
2025-03-10 11:46:06 +0100

    Eslint

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # ts-boilerplate
-Persistent browser cookies
+Typescript boilerplate
+
+Install
+
+```sh
+npm i
+```
+
+Format code
+
+```sh
+npm run format
+# prettier --write "src/**/*.ts" "dist/**/*.js"
+```
+
+Lint
+
+```sh
+npm run lint
+# npx eslint .
+```
+
+Build
+
+```sh
+npm run build
+# npx tsc
+```
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,53 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import eslint from '@eslint/js';
+import tsEslint from 'typescript-eslint';
 
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+    eslint.configs.recommended,
+    ...tsEslint.configs.recommendedTypeChecked,
+    {
+        languageOptions: {
+            ecmaVersion: 6,
+            sourceType: 'module',
+            parserOptions: {
+                ecmaFeatures: {
+                    modules: true,
+                },
+                tsconfigRootDir: '.',
+                projectService: true,
+            },
+        },
+        rules: {
+            '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+            '@typescript-eslint/explicit-function-return-type': 0,
+            '@typescript-eslint/explicit-member-accessibility': 0,
+            '@typescript-eslint/no-floating-promises': ['error', { ignoreIIFE: true, ignoreVoid: false }],
+            '@typescript-eslint/no-non-null-assertion': 'error',
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_', ignoreRestSiblings: true },
+            ],
+            '@typescript-eslint/require-await': 'error',
+            '@typescript-eslint/return-await': ['error', 'always'],
+            curly: 'error',
+            eqeqeq: 2,
+            'no-return-await': 'off',
+            'no-unused-expressions': 2,
+            'require-await': 'off',
+            'indent': ['error', 4],
+            '@typescript-eslint/no-explicit-any': 'error'
+        },
+    },
+    {
+        ignores: [
+            'node_modules',
+            'dist',
+        ],
+    },
+    {
+        /**
+         * Ignore configs and generated files
+         */
+        files: ['*.mjs', 'dist/**'],
+        ...tsEslint.configs.disableTypeChecked,
+    },
 ];
